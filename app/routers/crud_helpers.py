695ec6fa2d2fe1d8447771566207c8a169f5a76a
alisher <alisher200614bal@gmail.com>
2025-07-10 17:41:22 +0500

    modification 10.07.25

---
+++ app/routers/crud_helpers.py
@@ -0,0 +1,33 @@
+from typing import Literal, List
+from app.db.models import ToDo
+from fastapi import Query
+from logging import getLogger
+
+logger = getLogger()
+
+
+def handle_exception(e: Exception, message: str = "Ошибка сервера"):
+    logger.exception(message)
+    raise HTTPException(
+        status_code=500,
+        detail=f"{message}: {str(e)}"
+    )
+
+
+SortRule = Literal['date_desc', 'date_asc', 'name', 'status_false_first', 'status_true_first']
+
+todo_sort_mapping = {
+    'date_desc': ToDo.date_time.desc(),
+    'date_asc': ToDo.date_time.asc(),
+    'name': ToDo.name.asc(),
+    'status_false_first': ToDo.completion_status.asc(),
+    'status_true_first': ToDo.completion_status.desc()
+}
+
+
+def validate_sort(sort: List[SortRule] = Query(default=['date_desc'])) -> List[SortRule]:
+    if 'date_desc' in sort and 'date_asc' in sort:
+        raise ValueError("Нельзя использовать одновременно 'date_desc' и 'date_asc'")
+    if 'status_false_first' in sort and 'status_true_first' in sort:
+        raise ValueError("Нельзя использовать одновременно 'status_false_first' и 'status_true_first'")
+    return sort
