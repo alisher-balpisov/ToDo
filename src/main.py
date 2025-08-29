ee61c311c07c674a2de3f2990e8e5ec1f7f6437a
Alisherka2281337 <alisherbal228@outlook.com>
2025-08-29 20:43:50 +0500

    29.08.25

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
